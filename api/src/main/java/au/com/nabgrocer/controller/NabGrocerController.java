6c872f331c561802af33db104d57e77cf5e2384b
Chris Robertson <chrisxrobertson@gmail.com>
2019-07-24 19:16:44 +1000

    Refactored item endpoint to return item from database

--- api/src/main/java/au/com/nabgrocer/controller/NabGrocerController.java
+++ api/src/main/java/au/com/nabgrocer/controller/NabGrocerController.java
@@ -1,6 +1,10 @@
 package au.com.nabgrocer.controller;
 
 import au.com.nabgrocer.model.GroceryItem;
+import au.com.nabgrocer.service.GroceryItemRepositoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,17 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NabGrocerController {
 
-    @GetMapping("/item")
-    public GroceryItem item(@RequestParam(value="id") String id,
-                            @RequestParam(value="name", required = false) String name) {
+    private Logger log = LoggerFactory.getLogger(NabGrocerController.class);
 
-        GroceryItem groceryItem = new GroceryItem().setId(id);
+    private GroceryItemRepositoryService groceryItemRepositoryService;
 
-        if (name != null) {
-            groceryItem.setName(name);
-        }
-
-        return groceryItem;
+    @Autowired
+    public NabGrocerController(GroceryItemRepositoryService groceryItemRepositoryService) {
+        this.groceryItemRepositoryService = groceryItemRepositoryService;
     }
 
+    @GetMapping("/item")
+    public GroceryItem item(final @RequestParam(value = "name") String name) {
+        log.debug("'Get item by name request received' name_param='{}'", name);
+        return groceryItemRepositoryService.retrieveGroceryItemByName(name);
+    }
 }
