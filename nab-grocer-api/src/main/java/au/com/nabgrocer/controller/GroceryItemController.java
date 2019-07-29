94b491ca486d88d7a52f4eeeff8058a985fbce36
Chris Robertson <chrisxrobertson@gmail.com>
2019-07-29 10:42:47 +1000

    Javadoc comments for GroceryItem controller

--- nab-grocer-api/src/main/java/au/com/nabgrocer/controller/GroceryItemController.java
+++ nab-grocer-api/src/main/java/au/com/nabgrocer/controller/GroceryItemController.java
@@ -21,6 +21,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * RESTful CRUD endpoints for GroceryItems.
+ */
 @RestController
 public class GroceryItemController {
 
@@ -35,12 +38,25 @@ public class GroceryItemController {
         this.groceryItemService = groceryItemService;
     }
 
+    /**
+     * Retrieve an existing GroceryItem by its itemId.
+     *
+     * @param itemId The itemId to lookup
+     * @return the Grocery item retrieved from the database
+     */
     @GetMapping("/v1/items/{itemId}")
     public GroceryItem getItem(final @PathVariable("itemId") long itemId) {
         LOG.debug("'Get item by id request received' item_id='{}'", itemId);
         return groceryItemService.getGroceryItemById(itemId);
     }
 
+    /**
+     * Retrieve all GroceryItems that have a given tagId in their ItemTags.
+     *
+     * @param tagId The tagId to search by
+     * @return A list of GroceryItems containing given tagId
+     * @throws GroceryTagNotFoundException if tagId does not exist in database
+     */
     @GetMapping("/v1/items")
     public List<GroceryItem> getItemsByTagId(final @RequestParam("tagId") long tagId)
             throws GroceryTagNotFoundException {
@@ -50,6 +66,12 @@ public class GroceryItemController {
         return groceryItemService.getGroceryItemsByTag(tagId);
     }
 
+    /**
+     * Create a new GroceryItem and save it to the database.
+     *
+     * @param groceryItemDto GroceryItem to create
+     * @return The created GroceryItem
+     */
     @PostMapping("/v1/items")
     @ResponseStatus(HttpStatus.CREATED)
     public GroceryItem createItem(final @RequestBody GroceryItemDto groceryItemDto) {
@@ -58,8 +80,16 @@ public class GroceryItemController {
         return groceryItemService.insertGroceryItem(groceryItemToCreate);
     }
 
+    /**
+     * Update an existing GroceryItem in the database.
+     *
+     * @param groceryItemDto The GroceryItem with updated fields. Must contain itemId.
+     * @return The complete updated GroceryItem
+     * @throws IllegalArgumentException if request does not contain itemId
+     */
     @PutMapping("/v1/items")
-    public GroceryItem updateItem(final @RequestBody GroceryItemDto groceryItemDto) {
+    public GroceryItem updateItem(final @RequestBody GroceryItemDto groceryItemDto)
+            throws IllegalArgumentException {
         LOG.debug("'Update item request received' item_update='{}'", groceryItemDto);
         final GroceryItem groceryItemUpdate = mapToEntity(groceryItemDto);
 
@@ -71,6 +101,11 @@ public class GroceryItemController {
         return groceryItemService.updateGroceryItem(groceryItemUpdate);
     }
 
+    /**
+     * Delete an existing GroceryItem from the database.
+     * @param itemId The itemId of the GroceryItem to be deleted
+     * @throws GroceryItemNotFoundException if no GroceryItem exits for given itemId
+     */
     @DeleteMapping("/v1/items/{itemId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteItem(final @PathVariable int itemId) throws GroceryItemNotFoundException {
